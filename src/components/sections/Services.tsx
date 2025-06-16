d0d2df0c40bd2e8a8a94a927974e1e71da1757fc
Alvaro Marenco <alvaromarencom@gmail.com>
2025-06-15 18:45:02 -0600

    Services Section

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -13,7 +13,7 @@ export const Services = () => {
                     Our services include:
                 </Paragraph>
             </div>
-            <div>
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
                 {services.map((service, key) => (
                     <Service 
                         key={key}
