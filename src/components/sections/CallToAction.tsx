c731766e6566abd6bac583debb054c99ac1d8951
Alvaro Marenco <alvaromarencom@gmail.com>
2025-07-28 10:44:53 -0600

    Call To Action section updated

--- src/components/sections/CallToAction.tsx
+++ src/components/sections/CallToAction.tsx
@@ -2,16 +2,16 @@ import { Button, Container, Paragraph } from "../shared"
 
 export const CallToAction = () => {
   return (
-    <section className="pb-20 relative">
+    <section id="cta" className="pb-20 relative">
         <Container>
             <div className="relative rounded-2xl overflow-hidden">
                 <div className="relative z-10 mx-auto text-center max-w-xl md:max-w-2xl py-8 md:py-10 px-6 md:px-8">
-                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-heading-1"> Quick Start your <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-violet-600">own AI</span>  Business </h1>
+                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-heading-1"> Empieza a desarrolar tu <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-900 to-blue-900">proyecto con nosotros.</span></h1>
                     <Paragraph className="pt-10">
-                        Leverage our AI-powered platform to revolutionize your digital marketing efforts. Get data-driven insights and automation at your fingertips.
+                        Llena el siguiente formulario con tus datos y un breve comentario y muy pronto nos pondremos en contacto para brindarte una solución técnica.
                     </Paragraph>
                     <div className="pt-5 mx-auto max-w-md sm:max-w-xl dark:text-white">
-                        <Button className=" hover:bg-violet-900 hover:scale-105 hover:font-bold"> Get in touch </Button>
+                        {/* <Button className=" hover:bg-violet-900 hover:scale-105 hover:font-bold"> Get in touch </Button> */}
                     </div>
                 </div>
             </div>
