748442a06aab7fc63d8d29e58f9dc2275975f26a
Alvaro Marenco <alvaromarencom@gmail.com>
2025-08-11 12:15:39 -0600

    Numbers.tsx updated

--- src/components/sections/Numbers.tsx
+++ src/components/sections/Numbers.tsx
@@ -6,20 +6,20 @@ export const Numbers = () => {
         <Container className="flex justify-center items-center">
             <div className="mx-auto lg:mx-0 p-5 sm:p-6 sm:py-8 max-w-5xl rounded-3xl bg-box-bg border border-box-border shadow-lg shadow-box-shadow md:divide-x divide-box-border grid grid-cols-2 md:grid-cols-4">
                 <div className="text-center px-5">
-                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">100 +</h2>
-                    <p className="mt-2 text-heading-3">AI Models Implemented</p>
+                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">50 +</h2>
+                    <p className="mt-2 text-heading-3">Proyectos ejecutados</p>
                 </div>
                 <div className="text-center px-5">
-                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">250 +</h2>
-                    <p className="mt-2 text-heading-3">Enterprise Clients</p>
+                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">75 +</h2>
+                    <p className="mt-2 text-heading-3">Socios comerciales</p>
                 </div>
                 <div className="text-center px-5">
                     <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">99.9%</h2>
-                    <p className="mt-2 text-heading-3">Uptime Guarantee</p>
+                    <p className="mt-2 text-heading-3">Satisfacción Garantizada</p>
                 </div>
                 <div className="text-center px-5">
-                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">10 +</h2>
-                    <p className="mt-2 text-heading-3">Years of Innovation</p>
+                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">5 +</h2>
+                    <p className="mt-2 text-heading-3">Años de experiencia</p>
                 </div>
             </div>
         </Container>
