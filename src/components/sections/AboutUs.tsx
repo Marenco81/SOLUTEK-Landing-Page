1698f154c5fb7f1f33f82541e0a6f9a6eb6cfd5a
Alvaro Marenco <alvaromarencom@gmail.com>
2025-08-11 12:37:58 -0600

    image path test

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -1,6 +1,6 @@
 import { Info } from "../cards"
 import { Container, Paragraph, Title } from "../shared";
-import acerca from '../../assets-src/images/AcercaDe.jpg';
+// import acerca from '../../assets-src/images/AcercaDe.jpg';
 
 export const AboutUs = () => {
   return (
@@ -8,11 +8,11 @@ export const AboutUs = () => {
         <Container className="flex flex-col md:flex-row gap-10 lg:gap-12 items-center">
             <div className="w-full md:w-5/12 lg:w-1/2"> 
                 <div className="w-full h-80 sm:h-96 relative">
-                    <img 
+                    {/* <img 
                         src={acerca} 
                         alt="About our mission" 
                         className="w-full h-full object-cover rounded-3xl shadow-lg relative z-10"
-                    />
+                    /> */}
                 </div>
             </div>
             <div className="w-full md:w-7/12 lg:w-1/2 flex flex-col ">
